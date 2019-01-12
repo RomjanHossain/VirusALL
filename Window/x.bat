8525a3ad2d9c7cd0f5c52ac4ccffffea46e986d2
Immanuel Garcia <34188635+Manny56@users.noreply.github.com>
2019-01-11 18:27:59 -0600

    Update x.bat

--- Window/x.bat
+++ Window/x.bat
@@ -5,5 +5,5 @@ start FullScreen.vbs
 color 02
 mode 1000
 type smartscreen.txt
-echo YOUR COMPUTER HAS BEEN HACKED!!! PRESS ANY KEY TO CONTIUE...
+echo YOUR COMPUTER HAS BEEN HACKED!!! PRESS ANY KEY TO CONTINUE...
 pause>nul
